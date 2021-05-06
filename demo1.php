d16e272da0dd679ae597761cb331fee36f760999
thinhcubi <thinhcubi27@gmail.com>
2021-05-06 10:19:24 +0700

    demo1.php

---
+++ demo1.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Thoi gian hien tai</title>
+</head>
+<body>
+<h1> Bay gio la : <?php echo date('Y-M-d h:m:s'); ?></h1>
+<?php date_default_timezone_set('Asia/Ho_Chi_Minh')  ?>
+</body>
+</html>
